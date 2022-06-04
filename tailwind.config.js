c42f903f948b19597d0f460a2021317315a7121b
yuda <yuda.muhamad@usetada.com>
2022-06-04 20:19:54 +0700

    init portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#475569',
+        secondary: '#64748b',
+        dark: '#0f172a',
+        linkedin: '#0A66C2',
+        instagram: '#E4405F',
+        outlook: '#0078D4',
+        nodejs: '#339933',
+        vuejs: '#4FC08D',
+        golang: '#00ADD8',
+        flutter: '#02569B',
+        postgresql: '#4169E1',
+        mongodb: '#47A248',
+      },
+      screen: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
